00ce63c282b3ed9098e433ef17d6b99b6d83ef2a
Jared Counts <jcounts@houstonpublicmedia.org>
2020-02-27 10:03:28 -0600

    HPM Series: add meta if series template is selected Add options for top banners

--- series/hpm_series.php
+++ series/hpm_series.php
@@ -10,14 +10,18 @@
 	}
 
 	function hpm_series_add_meta() {
-		add_meta_box(
-			'hpm-series-meta-class',
-			esc_html__( 'Series Category', 'example' ),
-			'hpm_series_meta_box',
-			'page',
-			'normal',
-			'core'
-		);
+		global $wp_query;
+		$template = get_post_meta( get_the_ID(), '_wp_page_template', true );
+		if ( preg_match( '/series/', $template ) ) :
+			add_meta_box(
+				'hpm-series-meta-class',
+				esc_html__( 'Series Category', 'example' ),
+				'hpm_series_meta_box',
+				'page',
+				'normal',
+				'core'
+			);
+		endif;
 	}
 
 	function hpm_series_meta_box( $object, $box ) {
@@ -52,22 +56,27 @@
 				'bottom' => '',
 				'twitter' => '',
 				'facebook' => '',
-				'order' => 'ASC'
+				'order' => 'ASC',
+				'banners' => [
+					'mobile' => '',
+					'tablet' => '',
+					'desktop' => ''
+				]
 			];
 		endif; ?>
 		<p><?PHP _e( "Select the category for this series", 'example' ); ?></p>
 <?php
-	wp_dropdown_categories([
-		'show_option_all' => __("Select One"),
-		'taxonomy'        => 'category',
-		'name'            => 'hpm-series-cat',
-		'orderby'         => 'name',
-		'selected'        => $hpm_series_cat,
-		'hierarchical'    => true,
-		'depth'           => 5,
-		'show_count'      => false,
-		'hide_empty'      => false,
-	]); ?>
+		wp_dropdown_categories([
+			'show_option_all' => __("Select One"),
+			'taxonomy'        => 'category',
+			'name'            => 'hpm-series-cat',
+			'orderby'         => 'name',
+			'selected'        => $hpm_series_cat,
+			'hierarchical'    => true,
+			'depth'           => 5,
+			'show_count'      => false,
+			'hide_empty'      => false,
+		]); ?>
 		<p><?PHP _e( "What order would you like the articles to be displayed in?", 'example' ); ?></p>
 		<label for="hpm-series-order"><?php _e( "Article Order:", 'example' ); ?></label>
 		<select name="hpm-series-order" id="hpm-series-order">
@@ -76,6 +85,42 @@
 		</select><br />
 		<p><?PHP _e( "Which story should appear first?", 'example' ); ?></p>
 		<?php echo $top_story; ?>
+		<p>&nbsp;</p>
+		<h3><?PHP _e( "Banner Images", 'hpm-podcasts' ); ?></h3>
+		<p>Use the buttons below to select your mobile, tablet, and desktop banner images</p>
+		<?php
+			$hpm_mobile_url = $hpm_tablet_url = $hpm_desktop_url = '';
+			if ( !empty( $hpm_series_embeds['banners']['mobile'] ) ) :
+				$hpm_mobile_temp = wp_get_attachment_image_src( $hpm_series_embeds['banners']['mobile'], 'medium' );
+				$hpm_mobile_url = ' style="background-image: url('.$hpm_mobile_temp[0].')"';
+			endif;
+			if ( !empty( $hpm_series_embeds['banners']['tablet'] ) ) :
+				$hpm_tablet_temp = wp_get_attachment_image_src( $hpm_series_embeds['banners']['tablet'], 'medium' );
+				$hpm_tablet_url = ' style="background-image: url('.$hpm_tablet_temp[0].')"';
+			endif;
+			if ( !empty( $hpm_series_embeds['banners']['desktop'] ) ) :
+				$hpm_desktop_temp = wp_get_attachment_image_src( $hpm_series_embeds['banners']['desktop'], 'medium' );
+				$hpm_desktop_url = ' style="background-image: url('.$hpm_desktop_temp[0].')"';
+			endif;
+		?>
+		<div class="hpm-show-banner-wrap">
+			<div class="hpm-show-banner">
+				<div class="hpm-show-banner-image" id="hpm-show-banner-mobile"<?php echo $hpm_mobile_url; ?>></div>
+				<button class="hpm-show-banner-select button button-primary" data-show="mobile">Mobile</button>
+				<input value="<?php echo $hpm_series_embeds['banners']['mobile']; ?>" type="hidden" id="hpm-show-banner-mobile-id" name="hpm-show-banner-mobile-id" />
+			</div>
+			<div class="hpm-show-banner">
+				<div class="hpm-show-banner-image" id="hpm-show-banner-tablet"<?php echo $hpm_tablet_url; ?>></div>
+				<button class="hpm-show-banner-select button button-primary" data-show="tablet">Tablet</button>
+				<input value="<?php echo $hpm_series_embeds['banners']['tablet']; ?>" type="hidden" id="hpm-show-banner-tablet-id" name="hpm-show-banner-tablet-id" />
+			</div>
+			<div class="hpm-show-banner">
+				<div class="hpm-show-banner-image" id="hpm-show-banner-desktop"<?php echo $hpm_desktop_url; ?>></div>
+				<button class="hpm-show-banner-select button button-primary" data-show="desktop">Desktop</button>
+				<input value="<?php echo $hpm_series_embeds['banners']['desktop']; ?>" type="hidden" id="hpm-show-banner-desktop-id" name="hpm-show-banner-desktop-id" />
+			</div>
+		</div>
+		<p>&nbsp;</p>
 		<h4>Embeds</h4>
 		<p>Any elements you include in this box will be placed below the article stream.</p>
 		<label for="hpm-series-embeds"><?php _e( "Embeds:", 'example' ); ?></label><br />
@@ -88,10 +133,62 @@
 		<p>Facebook embeds for sidebar</p>
 		<label for="hpm-series-embeds-facebook"><?php _e( "Facebook Embeds:", 'example' ); ?></label><br />
 		<textarea id="hpm-series-embeds-facebook" name="hpm-series-embeds-facebook" style="height: 200px; width: 100%;"><?php echo $hpm_series_embeds['facebook']; ?></textarea>
-	<?php }
+		<script>
+			function capitalizeFirstLetter(string) {
+				return string[0].toUpperCase() + string.slice(1);
+			}
+			jQuery(document).ready(function($){
+				$('.hpm-show-banner-select').click(function(e){
+					e.preventDefault();
+					var size = $(this).attr('data-show');
+					var frame = wp.media({
+						title: 'Choose Your ' + capitalizeFirstLetter(size) + ' Banner',
+						library: {type: 'image'},
+						multiple: false,
+						button: {text: 'Set ' + capitalizeFirstLetter(size) + ' Banner'}
+					});
+					frame.on('select', function(){
+						var sizes = frame.state().get('selection').first().attributes.sizes;
+						if ( typeof sizes.medium !== 'undefined' ) {
+							var thumb = sizes.medium.url;
+						} else {
+							var thumb = sizes.full.url;
+						}
+						var attachId = frame.state().get('selection').first().id;
+						$('#hpm-show-banner-'+size).css( 'background-image', 'url('+thumb+')' )
+						$('#hpm-show-banner-'+size+'-id').val(attachId);
+					});
+					frame.open();
+				});
+			});
+		</script>
+		<style>
+			.hpm-show-banner-wrap {
+				overflow: hidden;
+			}
+			.hpm-show-banner {
+				width: 20%;
+				padding: 1em;
+				float: left;
+				text-align: center;
+			}
+			.hpm-show-banner .hpm-show-banner-image {
+				height: 0;
+				width: 100%;
+				padding-bottom: calc(100% / 1.5);
+				background-repeat: no-repeat;
+				background-size: cover;
+				background-position: top center;
+				border: 1px dotted #bfbfbf;
+				margin-bottom: 0.5em;
+			}
+		</style>
+<?php
+	}
 
 	function hpm_series_save_meta( $post_id, $post ) {
-		if ( $post->post_type == 'page' ) :
+		$template = get_post_meta( $post_id, '_wp_page_template', true );
+		if ( $post->post_type == 'page' && preg_match( '/series/', $template ) ) :
 			if ( !isset( $_POST['hpm_series_class_nonce'] ) || !wp_verify_nonce( $_POST['hpm_series_class_nonce'], basename( __FILE__ ) ) ) :
 				return $post_id;
 			endif;
@@ -110,6 +207,9 @@
 			$hpm_series_embeds['twitter'] = ( isset( $_POST['hpm-series-embeds-twitter'] ) ? $_POST['hpm-series-embeds-twitter'] : '' );
 			$hpm_series_embeds['facebook'] = ( isset( $_POST['hpm-series-embeds-facebook'] ) ? $_POST['hpm-series-embeds-facebook'] : '' );
 			$hpm_series_embeds['order'] = ( isset( $_POST['hpm-series-order'] ) ? $_POST['hpm-series-order'] : 'ASC' );
+			$hpm_series_embeds['banner']['mobile'] = ( isset( $_POST['hpm-show-banner-mobile'] ) ? $_POST['hpm-show-banner-mobile'] : '' );
+			$hpm_series_embeds['banner']['tablet'] = ( isset( $_POST['hpm-show-banner-tablet'] ) ? $_POST['hpm-show-banner-tablet'] : '' );
+			$hpm_series_embeds['banner']['desktop'] = ( isset( $_POST['hpm-show-banner-desktop'] ) ? $_POST['hpm-show-banner-desktop'] : '' );
 
 			update_post_meta( $post_id, 'hpm_series_cat', $hpm_series_cat );
 			update_post_meta( $post_id, 'hpm_series_top', $hpm_series_top );
